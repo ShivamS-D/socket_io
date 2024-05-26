495d020cc25996e35c0a22219b62cf13e603dee8
Ashutoshdapakara <ashutoshdapakara0@gmail.com>
2024-05-26 10:51:16 +0530

    first

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+
+const io=require('socket.io')(3000,{
+  cors:{
+    origin:['http://localhost:8080']
+  }
+
+})
+io.on('connection',socket=>{
+  console.log(socket.id)
+ 
+  
+  socket.on('join-room',(room,cb)=>{
+    socket.join(room)
+    
+    cb('Joined Room '+room)
+      socket.on('send_message',(string,room)=>{
+        if (room === '') {
+          socket.broadcast.emit('receive-message', string);
+          console.log(`Broadcasting message globally: ${string}`);
+        } else {
+          socket.to(room).emit('receive-message',string);
+          console.log(string)
+          }
+        console.log(string)
+      })
+  })
+})
---
+++ public/main.js
@@ -0,0 +1,62 @@
+import {io} from 'socket.io-client'
+// const io=require('socket.io-client')
+const joinRoomButton=document.getElementById('room-button')
+const roomInput=document.getElementById('room-input')
+const messageInput=document.getElementById('message-input')
+
+const form=document.getElementById('form')
+const socket=io('http://localhost:3000');
+socket.on('connect',()=>{
+  displayMessage("you connected with id"+socket.id)
+})
+
+socket.on('receive-message',message=>{
+  displayReceivedMessage(message)
+})
+form.addEventListener('submit',e=>{
+  e.preventDefault();
+  const message=messageInput.value;
+  const room=roomInput.value;
+
+  if(message!=='') displayMessage(message)
+    socket.emit('send_message',message,room)
+  messageInput.value=''
+})
+
+joinRoomButton.addEventListener('click',()=>{
+  const room=roomInput.value;
+  socket.emit('join-room',room,(message)=>{
+    displayMessage(message)
+  })
+})
+
+function displayMessage(message){
+  const div=document.createElement('div')
+  div.style.width='96vw'
+  const p=document.createElement('p')
+  div.appendChild(p)
+  p.textContent=message
+  p.style.width='50vw'
+  p.style.padding='0px'
+  p.style.whiteSpace = 'normal';
+p.style.wordWrap = 'break-word';
+  document.getElementById('message-container').append(div);
+ 
+}
+
+function displayReceivedMessage(message){
+  const div=document.createElement('div')
+  div.style.width='96vw'
+  const p=document.createElement('p')
+  p.textContent=message
+  div.appendChild(p)
+  p.style.backgroundColor='#02F9C9';
+  p.style.width='50vw'
+  p.style.padding='10px'
+  p.style.marginLeft='0vw'
+  p.style.whiteSpace = 'normal';
+p.style.wordWrap = 'break-word';
+  
+  document.getElementById('message-container').append(div);
+
+}

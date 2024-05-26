376fb2d0a4716729e0306d16fbded1e50a381538
Ashutoshdapakara <ashutoshdapakara0@gmail.com>
2024-05-26 11:28:13 +0530

    added

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
+const { io } = require("socket.io-client")
+
 // import {io} from 'socket.io-client'
-const {io}=require('socket.io-client')
 // const io=require('socket.io-client')
 const joinRoomButton=document.getElementById('room-button')
 const roomInput=document.getElementById('room-input')
